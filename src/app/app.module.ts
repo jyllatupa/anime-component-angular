fdb046893cd83f962f10e82e5beb0f893e83511e
jyllatupa <chelsea_30_01@hotmail.com>
2024-07-23 13:08:01 -0500

    anime tracker, comunicacion entre componentes con servicios y observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchAnimeComponent } from './components/search-anime/search-anime.component';
+import { ResultAnimeComponent } from './components/result-anime/result-anime.component';
+import { SelectedAnimeComponent } from './components/selected-anime/selected-anime.component';
+
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchAnimeComponent,
+    ResultAnimeComponent,
+    SelectedAnimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //con eso reconoce el ngModel
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/result-anime/result-anime.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Anime, MyAnime } from 'src/app/interfaces/api-movies';
+import { AnimeService } from 'src/app/services/anime.service';
+
+@Component({
+  selector: 'app-result-anime',
+  templateUrl: './result-anime.component.html',
+  styleUrls: ['./result-anime.component.css']
+})
+export class ResultAnimeComponent implements OnInit, OnDestroy{
+  anime_results: Anime[] = [];
+  animeSuscription!: Subscription;
+
+  constructor(private animeService: AnimeService){}
+
+  ngOnInit(): void {
+      this.animeSuscription = this.animeService.getResultAnime().subscribe(result => {
+        console.log('estoy en el componente result-anime: ', result);
+        this.anime_results = result;
+      })
+  }
+
+  ngOnDestroy(): void {
+      this.animeSuscription.unsubscribe();
+  }
+
+  addAnime(anime:Anime){
+    const addAnime: MyAnime = {
+      id: anime.mal_id,
+      title: anime.title,
+      imagen: anime.images['jpg'].image_url,
+      total_episodes: anime.episodes,
+      watched_espisodes: 0
+    }
+
+    this.animeService.animeSelected(addAnime);
+    this.anime_results = [];
+  }
+}
